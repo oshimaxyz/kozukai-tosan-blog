fb359901ece72c001833fdb64ca14db68c0332bf
oshima <oshimaxyz@gmail.com>
2025-08-11 21:09:34 +0900

    fix: Read Sanity API token from environment variable in frontend

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -5,5 +5,5 @@ export const client = createClient({
   dataset: 'production', // Your dataset name
   apiVersion: '2025-08-10', // Use a recent date
   useCdn: false, // Set to false for authenticated writes
-  token: 'skRhr6SFe1frTgUDlKdNRMaSMJ3A5LtuiQtfldxR3q6GGVv0nFPpB2mCsmsjMljpWtfL5wWr0sdUFlX0EAaXJOBDYnsg5qOzEGxq6r2RnTxlWTHWTCSs0LqFBj2U17A0emgcGTe9PqSX2Y4n4AgrDYD4qdI3H3aGloMoV7UYJiiWypfVkBmo', // Your API token
+  token: process.env.SANITY_API_TOKEN, // Your API token
 });
